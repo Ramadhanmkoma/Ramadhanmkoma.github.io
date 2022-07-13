e0569a9a047e419f7b8b634206f0c5124fbf785d
Ramadhan Mkoma <Ramadhanmkoma9@gmail.com>
2022-07-13 18:11:42 +0300

    Navigation Bar Added

--- js/javascript.js
+++ js/javascript.js
@@ -1,2 +1,14 @@
 //"use strict"
 const projectTitle = "Ramadhan Mohammed Mkoma";
+
+// alert("Welcome");
+
+document.addEventListener('DOMContentLoaded', () =>{
+  console.log("page loaded")
+
+  let roles = document.getElementById('role');
+
+      roles.addEventListener('click', () => {
+        alert("Roles");
+      });
+});
