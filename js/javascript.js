7ffa71bfb1dc049c9d4b0403dbd4ea4838f20c07
Ramadhan Mkoma <Ramadhanmkoma9@gmail.com>
2022-08-02 15:30:59 +0300

    Some Changeds

--- js/javascript.js
+++ js/javascript.js
@@ -1,14 +1,17 @@
 //"use strict"
 const projectTitle = "Ramadhan Mohammed Mkoma";
 
-// alert("Welcome");
+let showImage = false;
 
-document.addEventListener('DOMContentLoaded', () =>{
-  console.log("page loaded")
-
-  let roles = document.getElementById('role');
-
-      roles.addEventListener('click', () => {
-        alert("Roles");
-      });
+document.addEventListener('DOMContentLoaded', () => {
+  // console.log("page loaded")
+  document.getElementById('clickable-img').addEventListener('click', () => {
+    if (showImage == false) {
+      document.getElementById('fluid-image').classList.remove('hidden');
+      showImage = true;
+    } else {
+      document.getElementById('fluid-image').classList.add('hidden');
+      showImage = !showImage;
+    }
+  }); 
 });
