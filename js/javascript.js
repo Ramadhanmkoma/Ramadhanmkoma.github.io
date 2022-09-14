6655d2fbf9bb25f83d124055c5ead374a3837ace
Ramadhan Mkoma <Ramadhanmkoma9@gmail.com>
2022-09-14 21:00:34 +0300

    summary added

--- js/javascript.js
+++ js/javascript.js
@@ -1,14 +1,42 @@
 //"use strict"
 const projectTitle = "Ramadhan Mohammed Mkoma";
+let isAsideNavActive = true;
+
 
 let showImage = false;
 
 document.addEventListener('DOMContentLoaded', () => {
   setNavBarAside();
-});
+}); 
 
 let setNavBarAside = () => {
-  document.getElementById('menu').addEventListener('click', function () {
-    alert("nav bar clicked");
-  });
+  document.getElementById('menu').addEventListener('click', myNavHeadSec);
 };
+
+// Controlling Navigation Section
+let myNavHeadSec = () => {
+  if (isAsideNavActive) {
+      let aside = document.querySelector('aside').style.cssText = `
+        margin-left: -210px;
+        transition: .5s ease-in;
+      `;
+      let myheadTitle = document.getElementById('mytitle');
+          myheadTitle.style.cssText = `
+            margin-left: 0;
+            transition: .5s ease-in;
+          `;
+      isAsideNavActive = false;
+  } else {
+    let aside = document.querySelector('aside').style.cssText = `
+      margin-left: 0;
+      transition: .5s ease-in;
+    `;
+    let myheadTitle = document.getElementById('mytitle');
+        myheadTitle.style.cssText = `
+          margin-left: 0;
+          width: 100%;
+          transition: .5s ease-in;
+        `;
+    isAsideNavActive = true;
+  }
+}
