e53c2e94a9042ac53399e663782f996ffa74ebab
Ramadhan Mkoma <Ramadhanmkoma9@gmail.com>
2022-09-17 12:36:34 +0300

    side-nav text-alignment fixed

--- js/javascript.js
+++ js/javascript.js
@@ -1,41 +1,41 @@
-//"use strict"
-const projectTitle = "Ramadhan Mohammed Mkoma";
-let isAsideNavActive = true;
+// //"use strict"
+// const projectTitle = "Ramadhan Mohammed Mkoma";
+// let isAsideNavActive = true;
 
 
-let showImage = false;
+// let showImage = false;
 
-document.addEventListener('DOMContentLoaded', () => {
-  setNavBarAside();
-}); 
+// document.addEventListener('DOMContentLoaded', () => {
+//   setNavBarAside();
+// }); 
 
-let setNavBarAside = () => {
-  document.getElementById('menu').addEventListener('click', myNavHeadSec);
-};
+// let setNavBarAside = () => {
+//   document.getElementById('menu').addEventListener('click', myNavHeadSec);
+// };
 
-// Controlling Navigation Section
-let myNavHeadSec = () => {
-  if (isAsideNavActive) {
-      let aside = document.querySelector('aside').style.cssText = `
-        margin-left: -210px;
-        transition: .5s ease-in;
-      `;
-      let myheadTitle = document.getElementById('mytitle');
-          myheadTitle.style.cssText = `
-            margin-left: 0;
-            transition: .5s ease-in;
-          `;
-      isAsideNavActive = false;
-  } else {
-    let aside = document.querySelector('aside').style.cssText = `
-      margin-left: 0;
-      transition: .5s ease-in;
-    `;
-    let myheadTitle = document.getElementById('mytitle');
-        myheadTitle.style.cssText = `
-          margin-left: 0;
-          transition: .5s ease-in;
-        `;
-    isAsideNavActive = true;
-  }
-}
+// // Controlling Navigation Section
+// let myNavHeadSec = () => {
+//   if (isAsideNavActive) {
+//       let aside = document.querySelector('aside').style.cssText = `
+//         margin-left: -210px;
+//         transition: .5s ease-in;
+//       `;
+//       let myheadTitle = document.getElementById('mytitle');
+//           myheadTitle.style.cssText = `
+//             margin-left: 0;
+//             transition: .5s ease-in;
+//           `;
+//       isAsideNavActive = false;
+//   } else {
+//     let aside = document.querySelector('aside').style.cssText = `
+//       margin-left: 0;
+//       transition: .5s ease-in;
+//     `;
+//     let myheadTitle = document.getElementById('mytitle');
+//         myheadTitle.style.cssText = `
+//           margin-left: 0;
+//           transition: .5s ease-in;
+//         `;
+//     isAsideNavActive = true;
+//   }
+// }
