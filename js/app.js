ebb1b602533ca2e4a37f698cc9a4ead50c01738e
Ramadhan Mkoma <Ramadhanmkoma9@gmail.com>
2022-12-31 12:10:55 +0300

    service workers offline mode features added

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 if ('serviceWorker' in navigator ) {
 
     // get the sw file and register it as a service worker
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('sw.js')
     .then((reg) => console.log('service worker registered', reg))
     .catch((err) => console.log('service worker not registered', err))
     // that is a promise syntax, a way to deal with asynchronous task
